7e61e63702bc68bf74811f6ca8128c601f60776e
Rafael <rafaelbrandaobastos@gmail.com>
2024-04-04 15:50:35 -0300

    feat: adding graph exporter

--- graph/lib/exporter.ex
+++ graph/lib/exporter.ex
@@ -1,4 +1,34 @@
 defmodule Exporter do
-  def function do
+  def print_to_dot(graph, filename) do
+    full_path = "C:/Users/rafae/OneDrive/Área de Trabalho/ElixirGraph/#{filename}"
+    File.write!(full_path, generate_dot_content(graph))
+    IO.puts("Arquivo #{full_path} gerado com sucesso!")
+  end
+
+  defp generate_dot_content(graph) do
+    nodes =
+      Enum.map(graph.vertices, fn {vertex, _} ->
+        IO.puts("Visiting vertex: #{vertex}")
+        neighbors = get_neighbors(graph, vertex)
+
+        edges =
+          Enum.map(neighbors, fn neighbor ->
+            "#{vertex} -> #{neighbor}[penwidth=4];"
+          end)
+
+        [
+          "#{vertex} [label=\"#{vertex}\", fillcolor=\"lightblue\", style=\"filled\", shape=\"circle\"];",
+          edges
+        ]
+      end)
+      |> List.flatten()
+
+    "digraph G {\n" <>
+      "#{Enum.join(nodes, "\n  ")}\n" <>
+      "}\n"
+  end
+
+  defp get_neighbors(%{edges: edges}, vertex) do
+    Map.get(edges, vertex, [])
   end
 end
--- graph/lib/main.ex
+++ graph/lib/main.ex
@@ -1,14 +1,17 @@
 defmodule Main do
   # Importa as funções do módulo Graph
   import Graph
+  import Exporter
 
   def run do
     # Cria um novo grafo vazio
     graph = %Graph{}
-    graph_with_vertices = add_vertices(graph, 10)
-    graph_with_edges = add_edges(graph_with_vertices, 10, 10)
+    graphsize = 10
+    graph_with_vertices = add_vertices(graph, graphsize)
+    graph_with_edges = add_edges(graph_with_vertices, graphsize, graphsize)
     traverse(graph_with_edges)
     traverse_neighbors(graph_with_edges)
+    Exporter.print_to_dot(graph_with_edges, "graph.dot")
   end
 
   # Função para adicionar vértices ao grafo recursivamente
@@ -26,38 +29,36 @@ defmodule Main do
   # Função para adicionar arestas ao grafo recursivamente
   # Caso base: quando n é igual a 1
   def add_edges(graph, 1, i) do
-    random = :rand.uniform(i)
-
-    if random == 1 do
-      random = random
-    end
+    random = generate_random(1, i)
 
     randomS = Integer.to_string(random)
     updated_graph = add_edge(graph, "1", "2")
     add_edge(updated_graph, "1", "#{randomS}")
   end
 
+  # Gera numero randomico
+  defp generate_random(n, i) do
+    random = :rand.uniform(i)
+
+    if random == n do
+      generate_random(n, i)
+    else
+      random
+    end
+  end
+
   # Caso recursivo: quando n é maior que 1
   def add_edges(graph, n, i) when n > 0 do
-    if n == 10 do
+    if n == i do
       prev = n - 1
-      random = :rand.uniform(i)
-
-      if random == n do
-        random = random
-      end
-
+      random = generate_random(n, i)
       updated_graph = add_edge(graph, Integer.to_string(n), Integer.to_string(prev))
       updated_graph2 = add_edge(updated_graph, Integer.to_string(n), Integer.to_string(random))
       add_edges(updated_graph2, n - 1, i)
     else
       prev = n - 1
       next = n + 1
-      random = :rand.uniform(i)
-
-      if random == n do
-        random = random
-      end
+      random = generate_random(n, i)
 
       updated_graph = add_edge(graph, Integer.to_string(n), Integer.to_string(prev))
       updated_graph2 = add_edge(updated_graph, Integer.to_string(n), Integer.to_string(next))
